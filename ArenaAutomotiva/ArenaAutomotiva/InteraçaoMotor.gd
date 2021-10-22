469fbb10c5d0ba1135a74fc39a9948ce61d1036e
manuziny <88348637+manuziny@users.noreply.github.com>
2021-10-21 23:14:29 -0300

    Add files via upload

---
+++ ArenaAutomotiva/ArenaAutomotiva/InteraçaoMotor.gd
@@ -0,0 +1,21 @@
+extends Area2D
+
+var interativo = false
+const dialog = preload("res://CaixasDeTexto/Warning_Arena_Automotiva2.tscn")
+
+
+func _on_InteraaoMotor_body_entered(body):
+	if body.name == "Player":
+		$Label.visible = true
+		interativo = true
+
+func _on_InteraaoMotor_body_exited(body):
+	if body.name == "Player":
+		$Label.visible = false
+		interativo = false
+
+func _input(event):
+	if Input.is_key_pressed(KEY_E) and interativo == true:
+		$Label.visible = false
+		var dialogo = dialog.instance()
+		get_parent().add_child(dialogo)
